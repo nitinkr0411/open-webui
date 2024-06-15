0751c13c9097cd8398128c838e35fb6c0c3f8906
rionitinkumar <nitin.kumar@riotinto.com>
2024-06-15 00:34:46 -0700

    add verify_signature as false as alb is doing it anyways

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -61,22 +61,7 @@ def decode_token(token: str) -> Optional[dict]:
     
 def decode_token_from_alb(token: str) -> Optional[dict]:
     try:
-        encoded_jwt = token
-        jwt_headers = encoded_jwt.split('.')[0]
-        decoded_jwt_headers = base64.b64decode(jwt_headers)
-        decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
-        decoded_json = json.loads(decoded_jwt_headers)
-
-        # Step 2: Get the key id from JWT headers (the kid field)
-        kid = decoded_json['kid']
-
-        # Step 3: Get the public key from regional endpoint
-        url = 'https://public-keys.auth.elb.ap-southeast-2.amazonaws.com/' + kid
-        req = requests.get(url)
-        pub_key = req.text
-
-        # Step 4: Get the payload
-        payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES256'])
+        payload = jwt.decode(token, options={"verify_signature": False})
         logging.info(payload)
         return payload
     except Exception as e:
